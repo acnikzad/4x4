81dc4069face87cd33d8b0644b19a4ccd4cffe45
“Aria <a.c.nikzad@gmail.com>
2020-11-04 09:14:31 -0800

    started homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,112 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section id="features">
+      <div class="container">
+        <header>
+          <h2>Gentlemen, behold! This is <strong>Strongly Typed</strong>!</h2>
+        </header>
+        <div class="row aln-center">
+          <div class="col-4 col-6-medium col-12-small">
+<!-- Feature -->
+            <section>
+              <a href="#" class="image featured"><img src="images/XTR1.jpg" alt="" /></a>
+              <header>
+                <h3>XTR</h3>
+              </header>
+              <p>This is <strong>Strongly Typed</strong>, a free, fully responsive site template
+              by <a href="http://html5up.net">HTML5 UP</a>. Free for personal and commercial use under the
+              <a href="http://html5up.net/license">CCA 3.0 license</a>.</p>
+            </section>
+          </div>
+          <div class="col-4 col-6-medium col-12-small">
+<!-- Feature -->
+            <section>
+              <a href="#" class="image featured"><img src="images/XTC1.jpg" alt="" /></a>
+              <header>
+                <h3>XTR-XL</h3>
+              </header>
+              <p><a href="http://html5up.net">HTML5 UP</a> is a side project of <a href="http://twitter.com/ajlkn">AJ’s</a> (= me).
+              I started it as a way to both test my responsive tools and sharpen up my coding
+              and design skills a bit.</p>
+            </section>
+          </div>
+          <div class="col-4 col-6-medium col-12-small">
+<!-- Feature -->
+            <section>
+              <a href="#" class="image featured"><img src="images/IMG_9945.jpg" alt="" /></a>
+              <header>
+                <h3>XTR-Toy Hauler</h3>
+              </header>
+              <p><strong>Responsive Tools</strong> is a simple set of tools for building responsive
+              sites and apps. All of my templates at <a href="http://html5up.net">HTML5 UP</a> are built using these tools.</p>
+            </section>
+          </div>
+        </div>
+        <br>
+        <div class="row aln-center">
+          <div class="col-4 col-6-medium col-12-small">
+<!-- Feature -->
+            <section>
+              <a href="#" class="image featured"><img src="images/XTR8.jpg" alt="" /></a>
+              <header>
+                <h3>XTC</h3>
+              </header>
+              <p>This is <strong>Strongly Typed</strong>, a free, fully responsive site template
+              by <a href="http://html5up.net">HTML5 UP</a>. Free for personal and commercial use under the
+              <a href="http://html5up.net/license">CCA 3.0 license</a>.</p>
+            </section>
+          </div>
+          <div class="col-4 col-6-medium col-12-small">
+<!-- Feature -->
+            <section>
+              <a href="#" class="image featured"><img src="images/XTC3.JPG" alt="" /></a>
+              <header>
+                <h3>Command Camp</h3>
+              </header>
+              <p><a href="http://html5up.net">HTML5 UP</a> is a side project of <a href="http://twitter.com/ajlkn">AJ’s</a> (= me).
+              I started it as a way to both test my responsive tools and sharpen up my coding
+              and design skills a bit.</p>
+            </section>
+          </div>
+          <div class="col-4 col-6-medium col-12-small">
+<!-- Feature -->
+            <section>
+              <a href="#" class="image featured"><img src="images/IMG_9938.JPG" alt="" /></a>
+              <header>
+                <h3>XSport</h3>
+              </header>
+              <p><strong>Responsive Tools</strong> is a simple set of tools for building responsive
+              sites and apps. All of my templates at <a href="http://html5up.net">HTML5 UP</a> are built using these tools.</p>
+            </section>
+          </div>
+          <div class="col-12">
+            <ul class="actions">
+              <li><a href="#" class="button icon solid fa-file">Tell Me More</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+<!-- Banner -->
+    <section id="banner">
+      <div class="container">
+        <p>WHY <strong>OUR</strong> TRAILERS?</p>
+      </div>
+    </section>
+    <section id="points">
+      <div class="container row">
+        <div class="col-4 col-6-medium col-12-small">
+          <p>1/3</p>
+        </div>
+        <div class="col-4 col-6-medium col-12-small">
+          <p>2/3</p>
+        </div>
+        <div class="col-4 col-6-medium col-12-small">
+          <p>3/3</p>
+        </div>
+      </div>
+    </section>
+    
   </div>
 </template>
 
